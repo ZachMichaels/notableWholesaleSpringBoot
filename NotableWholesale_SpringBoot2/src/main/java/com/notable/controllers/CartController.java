8b6787b38095809c3d541d635a088bcd534692eb
steve <steve@DESKTOP-PN38NAL>
2020-05-19 10:40:06 -0700

    Fleshed out cartController with some methods

--- NotableWholesale_SpringBoot2/src/main/java/com/notable/controllers/CartController.java
+++ NotableWholesale_SpringBoot2/src/main/java/com/notable/controllers/CartController.java
@@ -1,5 +1,49 @@
 package com.notable.controllers;
 
-public class CartController {
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.notable.business.Cart;
+import com.notable.business.User;
+
+@Controller
+public class CartController {
+	
+	@GetMapping("cart")
+	public String showCart() {
+		return "/views/cart";
+	}
+	
+	@PostMapping("addItem")
+	public void addToCart(HttpServletRequest request, HttpServletResponse response) {
+		//if cart doesn't exist then create the cart
+		HttpSession session = request.getSession();
+        User user = (User) session.getAttribute("user");
+        Cart cart = (Cart) session.getAttribute("cart");
+        if (cart == null && user == null) {
+            request.setAttribute("emptyCart", "Your cart is empty");
+        } else {
+        	//Read cookies for cart here.
+        	request.removeAttribute("emptyCart");
+            request.getSession().setAttribute("cart", cart);
+        }
+        showCart();
+	}
+	
+	@PostMapping("removeItem")
+	public void removeFromCart() {
+		
+	}
+	
+	@PostMapping("updateItem")
+	public void updateCart() {
+		
+	}
 }
